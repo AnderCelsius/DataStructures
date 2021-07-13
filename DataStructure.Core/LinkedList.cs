00b92299caeb8b7590eb41153ab1697b3fab1c9c
obinna asiegbulam <oasiegbulam@gmail.com>
2021-07-13 04:02:44 +0100

    Add project files.

---
+++ DataStructure.Core/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using DataStructure.Model;
+using System;
+
+namespace DataStructure.Core
+{
+    /// <summary>
+    /// Creates the linkage for the nodes of the linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LinkedList<T>
+    {
+        private Node<T> Head;
+        private Node<T> Tail;
+        private int Size;
+
+        /// <summary>
+        /// Starts out with its data set as null until an item is added to it
+        /// </summary>
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+
+        /// <summary>
+        /// returns the number of items in the list.
+        /// </summary>
+        /// <returns></returns>
+        public int Length()
+        {
+            return Size;
+        }
+
+        /// <summary>
+        /// checks if the list is empty and returns either true or false
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+
+        /// <summary>
+        /// Adds item to the linked list and returns the current size of the list.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public int Add(T item)
+        {
+            var newest = new Node<T>(item, null);
+            if (IsEmpty())
+                Head = newest;
+            else
+            {
+                Tail.Next = newest;
+            }
+            Tail = newest;
+            Size++;
+            return Size;
+        }
+    }
+}
---
+++ DataStructure.Model/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructure.Model
+{
+    public class Node<T>
+    {
+        public T Data;
+        public Node<T> Next;
+
+        public Node(T data, Node<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
+
+    }
+}
