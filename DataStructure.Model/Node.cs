d8b3adb3834ee9276505ea147481bfcc51cea974
obinna asiegbulam <oasiegbulam@gmail.com>
2021-07-14 09:00:18 +0100

    Added comments to Node class

--- DataStructure.Model/Node.cs
+++ DataStructure.Model/Node.cs
@@ -2,6 +2,10 @@
 
 namespace SinglyLinkedList
 {
+    /// <summary>
+    /// Holds the data value and the pointer which points to the location of the next node in the linked list. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Node<T> 
     {
         public T Data;
